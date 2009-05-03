55132687917dbd3125a4250a72dc66c7040e04b0
siuying <siu.ying@gmail.com>
2009-05-03 13:37:21 +0800

    init commit

---
+++ lib/test_similar_1.rb
@@ -0,0 +1,10 @@
+require 'dm/similar'
+require 'yaml'
+
+data = YAML.load(File.open("../config/data.yml"))
+d1 = data["Lisa Rose"]
+d2 = data["Gene Seymour"]
+
+eucli = Dm::Similar::EuclideanDistance.new
+
+puts eucli.distance(d1, d2)
