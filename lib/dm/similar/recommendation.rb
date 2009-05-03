b77a3ff176c6616e23ad217e987605686b6028bf
siuying <siu.ying@gmail.com>
2009-05-03 18:41:53 +0800

    add test case showing how to reverse the recommendation - finding products that are similar, and recommend product to people

--- lib/dm/similar/recommendation.rb
+++ lib/dm/similar/recommendation.rb
@@ -2,12 +2,12 @@ module Dm
   module Similar
     class Recommendation
       # find others people that most similar to specified person
-      def top_matches(data, person, n = 5, metric = Pearson.new)
+      def top_matches(data, key, n = 5, metric = Pearson.new)
         raise StandardError.new("n must be positive integer") if n < 1
 
-        others = data.keys - [person]
+        others = data.keys - [key]
         scores = others.collect do |other|
-          [metric.distance(data, person, other), other] 
+          [metric.distance(data, key, other), other]
         end
 
         # sort and reverse the array
@@ -20,19 +20,19 @@ module Dm
 
       # get recommended items: items not rated and likely rate highly by this person  
       # based on similarity of this person to others and their rating
-      def get_recommendation(data, person, metric = Pearson.new)
+      def get_recommendation(data, key, metric = Pearson.new)
         totals = {}
         sim_sums = {}
 
-        others = data.keys - [person]
+        others = data.keys - [key]
         others.each do |other|
-          sim = metric.distance(data, person, other)
+          sim = metric.distance(data, key, other)
 
           # only check others with similarity > 0
           if sim > 0
             data[other].each do |item, score|
               # only check item not rated by me
-              if data[person][item].nil? or data[person][item] == 0
+              if data[key][item].nil? or data[key][item] == 0
                 totals[item] = 0 if totals[item].nil?
                 sim_sums[item] = 0 if sim_sums[item].nil?
 
--- lib/test_rec_2.rb
+++ lib/test_rec_2.rb
@@ -5,4 +5,6 @@ data = YAML.load(File.open("../config/data.yml"))
 
 data_r = Dm::Similar.transform_pref(data)
 
-puts data_r.inspect
+rec = Dm::Similar::Recommendation.new
+puts rec.top_matches(data_r, "Superman Returns").inspect
+puts rec.get_recommendation(data_r, "Just My Luck").inspect
